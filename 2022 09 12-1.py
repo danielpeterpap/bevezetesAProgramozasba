789f7d0de50e5b6e34bec5e9a9bb09ba4357698a
danielpeterpap <43390985+danielpeterpap@users.noreply.github.com>
2022-09-12 09:25:48 +0200

    modified:   2022 09 12-1.py Class ended

--- 2022 09 12-1.py
+++ 2022 09 12-1.py
@@ -36,4 +36,42 @@ print(a+b)
 
 ################################################################
 
+age= 26
+txt= "I am {}"
+print(txt.format(age))
+
+quantity=3
+Itemno = 564
+price=70.83
+myorder= "I want {} pieces of {} for {}"
+print(myorder.format(quantity,Itemno,price))
+
+###################################################################
+#print("adjon meg 2 egész számot!")
+#a=int(input())
+#b=int(input())
+#if a > b:
+#    print("a greater than b")
+#else:
+#    print("b greater than a")
+
+###############################
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango","melon"]
+
+print(thislist[2:5]) # 2-től 5 ig
+print(thislist[:4]) # 4 ig az elejétől
+print(thislist[-4:-1]) # hátulról kezd
+print(thislist[::-1]) # visszafele
+print(thislist[0:len(thislist):3]) # minden 3.
+
+#thislist[1:3] = ["valami", "cica"]
+#print(thislist)
+
+#thislist.insert(2,"watermelon")
+#print(thislist)
+
+thislist.append("watermelon")
+print(thislist)
+
 
