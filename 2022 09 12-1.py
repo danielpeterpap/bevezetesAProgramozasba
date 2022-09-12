be87b7c8ac1726ee14ead24b2d316e65f86f51b2
danielpeterpap <43390985+danielpeterpap@users.noreply.github.com>
2022-09-12 09:06:23 +0200

    modified:   2022 09 12-1.py Progress in class

--- 2022 09 12-1.py
+++ 2022 09 12-1.py
@@ -11,3 +11,29 @@ print(b)
 
 print(type(a))
 print(type(b))
+
+################################################################
+
+for x in "banana":
+    print(x)
+a="Hello world!"
+print(len(a))
+
+txt = "The best things in life are free!"
+
+if "free" in txt:
+    print("contain")
+print(txt.upper())
+print(txt.lower())
+
+a= "Hello, world!"
+print(a.split(' '))
+a="hello "
+b="world"
+c=a+b
+print(c)
+print(a+b)
+
+################################################################
+
+
