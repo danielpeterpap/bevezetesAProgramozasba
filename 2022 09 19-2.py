14f2c7430851f169a76d589e973a54bc2f6388bf
danielpeterpap <43390985+danielpeterpap@users.noreply.github.com>
2022-09-19 08:30:14 +0200

    new file:   2022 09 19-2.py 	modified:   2022 09 19.py

---
+++ 2022 09 19-2.py
@@ -0,0 +1,7 @@
+def calculate(name):
+    name=name.lower()
+    if name == "rectangle":
+        l= int(input("Enter the rectangle's length"))
+
+if __name__ == "__main__":
+    pass
---
+++ 2022 09 19.py
@@ -0,0 +1,32 @@
+numbers = [6,5,3,8,4,2,5,4,11]
+
+sum=0
+
+for i in numbers:
+    sum=sum+i
+print("sum is: ",sum)
+
+################################################################
+
+print(list(range(1,10)))
+print(list(range(3,10)))
+print(list(range(1,30,4)))
+
+################################################################
+
+genre= ["Pop", "Rock" ,"Jazz"]
+
+print(len(genre))
+
+for i in range(len(genre)):
+    print("I like ",genre[i])
+
+digits=[5,4,3,7,9,3,]
+
+for i in digits:
+    print(i)
+else:
+    print("No more digits")
+
+
+      
