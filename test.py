fdac8a330111b24295876672efbf474210f2914e
danielpeterpap <43390985+danielpeterpap@users.noreply.github.com>
2022-11-06 21:32:40 +0100

    deleted:    test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-mystring = "Hello Oraask"
-# Getting length of string 
-lengthOfmystring = len(mystring)
-# Iterating through the string using while loop 
-for i in mystring[0:lengthOfmystring:2] : 
-# Print all characters iterated
-    print("Element of string:" , i)
